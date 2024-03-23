14a5b09c903502c52c1cd9ab406cc621bb95e38d
scottXchoo <ckh0601@cau.ac.kr>
2024-03-23 15:17:04 +0900

    HelloJava.java

---
+++ src/variable/Var6.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class Var6 {
+    public static void main(String[] args) {
+        int a;
+        // System.out.println(a);
+        /* 초기화하지 않은 변수 읽기
+        컴퓨터에서 메모리는 여러 시스템이 함께 사용하는 공간
+        초기화를 하지 않으면 이상한 값이 출력될 수 있으므로 이를 자바에서 강제함
+        */
+    }
+}
