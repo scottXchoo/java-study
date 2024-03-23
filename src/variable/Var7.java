550d2d7d5a2626c902d67973b8f418e652a68ca0
scottXchoo <ckh0601@cau.ac.kr>
2024-03-23 15:17:04 +0900

    HelloJava.java

---
+++ src/variable/Var7.java
@@ -0,0 +1,30 @@
+package variable;
+
+public class Var7 {
+    public static void main(String[] args) {
+        // 리터럴(literal) : 코드에서 개발자가 직접 적은 고정된 값
+        int a = 100; // 정수
+        double b = 10.5; // 실수
+        boolean c = true; // 불리언(boolean)
+        char d = 'A'; // 문자 하나
+        String e = "Hello Java"; // 문자열; 문자열을 다루기 위한 특별한 타입
+
+        // 정수 : 기본 int
+        byte by = 127; // -128 ~ 127
+        short s = 32767; // -32,768 ~ 32,767
+        int i = 2147483647; // -2147483648 ~ 2147483647 (약 20억)
+        long l = 9213141221L; // 엄청 큰 범위
+
+        // 실수 : 기본 double
+        float f = 10.0f;
+        double dou = 10.0;
+        /*
+        실무에서 거의 사용 X : byte, short, float, char 등
+        자주 사용하는 타입
+        - 정수 : int, long
+        - 실수 : double
+        - 불린형 : boolean
+        - 문자열 : String
+         */
+    }
+}
